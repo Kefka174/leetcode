fd3f31ee6b592d8e6323f480f256d8dfa310aa25
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-04-04 08:34:10 -0500

    New solution - "206. Reverse Linked List"

---
+++ solutions/reverse-linked-list/solution.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+    if (head == nullptr) return head;
+    ListNode *listIndex = head->next;
+    ListNode *listNextIndex;
+    head->next = nullptr;
+
+    while (listIndex != nullptr) {
+        listNextIndex = listIndex->next;
+        listIndex->next = head;
+        head = listIndex;
+        listIndex = listNextIndex;
+    }
+    return head;
+}
+
+
+
+void printList(ListNode* head) {
+    ListNode *index = head;
+    while (index != nullptr) {
+        cout << index->val << ", ";
+        index = index->next;
+    }
+}
+
+int main() {
+    ListNode *c = new ListNode(3);
+    ListNode *b = new ListNode(2, c);
+    ListNode *a = new ListNode(1, b);
+    printList(reverseList(a));
+    return 0;
+}
