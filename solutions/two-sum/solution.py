9033ecca752192b7464c09e9216b77e3755ebd06
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-04-13 08:31:09 -0500

    Updated "Two Sum" to o(n)

--- solutions/two-sum/solution.py
+++ solutions/two-sum/solution.py
@@ -7,3 +7,10 @@ def twoSum(self, nums, target):
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target: return [i,j]
+
+def twoSumFaster(self, nums, target):
+    lookupDict = {}
+    for i, num in enumerate(nums):
+        secondNum = target - num
+        if lookupDict.get(secondNum) != None: return [i, lookupDict[secondNum]]
+        lookupDict[num] = i
