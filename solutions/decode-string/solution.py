096904d815bf5c5b167e0a3dca0a0ffc84d43859
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-04-16 12:24:37 -0500

    Added iterative solution to "Decode String"

--- solutions/decode-string/solution.py
+++ solutions/decode-string/solution.py
@@ -33,7 +33,33 @@ def calcRepeatingSubsequence(s, startingIndex):
     return sequenceList, i + 1
 
 
-assert decodeString("3[a]2[bc]") == "aaabcbc"
-assert decodeString("3[a2[c]]") == "accaccacc"
-assert decodeString("2[abc]3[cd]ef") == "abcabccdcdcdef"
-assert decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef") == "zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef"
+def decodeStringIterative(s):
+    i = 0
+    stack = [[]]
+    while i < len(s):
+        if s[i].isdigit():
+            multVal = int(s[i])
+            while s[i + 1] != '[': # loop to build multi-digit number
+                i += 1
+                multVal *= 10
+                multVal += int(s[i])
+            stack.append(multVal)
+        elif s[i] == '[':
+            stack.append([])
+        elif s[i] == ']':
+            sequence = stack.pop()
+            multVal = stack.pop()
+            print(sequence)
+            stack[-1] += sequence * multVal
+            print(stack)
+        else:
+            stack[-1].append(s[i])
+        i += 1
+        
+    return "".join(stack[0])
+
+
+assert decodeStringIterative("3[a]2[bc]") == "aaabcbc"
+assert decodeStringIterative("3[a2[c]]") == "accaccacc"
+assert decodeStringIterative("2[abc]3[cd]ef") == "abcabccdcdcdef"
+assert decodeStringIterative("3[z]2[2[y]pq4[2[jk]e1[f]]]ef") == "zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef"
