d4230c5d47219cfa490ac91d286d87ffc8ae304d
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-04-18 17:00:26 -0500

    Updated "Happy Number" to O(1) space

--- solutions/happy-number/solution.py
+++ solutions/happy-number/solution.py
@@ -14,6 +14,21 @@ def isHappy(n):
         seenDigits.add(happyN)
     return True
 
-assert isHappy(19) == True
-assert isHappy(2) == False
-assert isHappy(3) == False
+def nextHappy(n):
+    next = 0
+    for digit in str(n): next += int(digit)**2
+    return next
+
+def isHappyFloyds(n): # using Floyd's cycle detection algorithm
+    slow = n
+    fast = n
+    while True:
+        slow = nextHappy(slow)
+        fast = nextHappy(nextHappy(fast))
+        if fast == 1: return True
+        if slow == fast: return False
+
+assert isHappyFloyds(19) == True
+assert isHappyFloyds(2) == False
+assert isHappyFloyds(3) == False
+assert isHappyFloyds(10) == True
