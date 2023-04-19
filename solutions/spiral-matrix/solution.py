e1eb5c9de8e8ad0bae512061d1a170a09d5f85a3
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-04-18 22:07:31 -0500

    Refactored "Spiral Matrix"

--- solutions/spiral-matrix/solution.py
+++ solutions/spiral-matrix/solution.py
@@ -2,6 +2,28 @@
 :type matrix: List[List[int]]
 :rtype: List[int]
 """
+# cleaned up by re-writing matrix elements and using a travel direction list
+def spiralOrderCleaner(matrix):
+    returnList = []
+    indexCoords = [0, 0]
+    travelDirection = [1, 0]
+    
+    while len(returnList) < len(matrix[0]) * len(matrix):
+        indexVal = matrix[indexCoords[1]][indexCoords[0]]
+        nextCoords = [indexCoords[0] + travelDirection[0], indexCoords[1] + travelDirection[1]]
+        returnList.append(indexVal)
+        matrix[indexCoords[1]][indexCoords[0]] = 'x'
+        
+        indexAtEndOfLine = nextCoords[0] == len(matrix[0]) or nextCoords[1] == len(matrix) or nextCoords[0] < 0
+        if indexAtEndOfLine or matrix[nextCoords[1]][nextCoords[0]] == 'x':
+            travelDirection = [-travelDirection[1], travelDirection[0]] # rotate direction
+            nextCoords = [indexCoords[0] + travelDirection[0], indexCoords[1] + travelDirection[1]] # recalculate next coords
+        indexCoords = nextCoords
+    return returnList
+
+
+
+# uses shrinking travel distance counters and too many if-statements
 def spiralOrder(matrix):
     returnList = []
     indexCoords = [0, 0]
@@ -43,3 +65,6 @@ def spiralOrder(matrix):
                 direction = "right"
                 indexCoords[1] += 1
     return returnList
+
+assert spiralOrderCleaner([[1,2,3],[4,5,6],[7,8,9]]) == [1,2,3,6,9,8,7,4,5]
+assert spiralOrderCleaner([[1,2,3,4],[5,6,7,8],[9,10,11,12]]) == [1,2,3,4,8,12,11,10,9,5,6,7]
