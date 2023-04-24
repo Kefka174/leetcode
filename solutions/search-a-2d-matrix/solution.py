d134c740562b0b0fc6025990e375fb58d7aab795
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-04-24 16:38:16 -0500

    Refactored "74. Search a 2D Matrix"

--- solutions/search-a-2d-matrix/solution.py
+++ solutions/search-a-2d-matrix/solution.py
@@ -1,23 +1,20 @@
-from math import ceil
 """
 :type matrix: List[List[int]]
 :type target: int
 :rtype: bool
 """
 def searchMatrix(matrix, target):
-    searchAreaSize = (len(matrix) * len(matrix[0])) / float(2)
-    midIndex = int(ceil(searchAreaSize))
+    topIndex = (len(matrix) * len(matrix[0])) - 1
+    bottomIndex = 0
 
-    while searchAreaSize >= 0.5:
-        midRow, midCol = numToRowCol(midIndex - 1, len(matrix[0]))
-        if midCol >= len(matrix): return False
+    while bottomIndex <= topIndex:
+        midIndex = ((topIndex - bottomIndex) // 2) + bottomIndex
+        midRow, midCol = numToRowCol(midIndex, len(matrix[0]))
 
         if matrix[midCol][midRow] == target: return True
 
-        searchAreaSize /= 2
-        if target > matrix[midCol][midRow]: midIndex += int(ceil(searchAreaSize))
-        else: midIndex -= int(ceil(searchAreaSize))
-        
+        if target > matrix[midCol][midRow]: bottomIndex = midIndex + 1
+        else: topIndex = midIndex - 1
     return False
 
 def numToRowCol(num, rowLength):
@@ -36,5 +33,6 @@ assert searchMatrix([[1]], 1) == True
 assert searchMatrix([[1,4,6]], 4) == True
 assert searchMatrix([[2,4,6]], 1) == False
 assert searchMatrix([[1,3]], 3) == True
+assert searchMatrix([[1,3]], 1) == True
 assert searchMatrix([[-10,-9,-7,-6,-5],[-2,0,2,3,4],[5,6,8,9,9],[10,11,12,14,15]], 14) == True
 assert searchMatrix([[-10,-9,-7,-6,-5],[-2,0,2,3,4],[5,6,8,9,9],[10,11,12,14,15]], 25) == False
