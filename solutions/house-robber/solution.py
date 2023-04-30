c66f6438398f34d5918bad9c4b872b91dd29f1ae
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-04-30 18:58:04 -0500

    Updated "198. House Robber" to o(1) space

--- solutions/house-robber/solution.py
+++ solutions/house-robber/solution.py
@@ -3,23 +3,27 @@
 :rtype: int
 """
 def rob(nums):
-    maxFromPreviousHouses = [-1 for i in nums]
-    maxFromPreviousHouses[0] = nums[0]
+    curentMax = nums[0]
+    maxRobbingPreviousHouse = nums[0]
+    maxBeforePreviousHouse = 0
     currentHouse = 1
 
     while currentHouse < len(nums):
-        valRobbingCurrent = nums[currentHouse]
-        if currentHouse > 1: valRobbingCurrent += maxFromPreviousHouses[currentHouse - 2]
+        valRobbingCurrent = nums[currentHouse] + maxBeforePreviousHouse
         
-        valPassingCurrent = maxFromPreviousHouses[currentHouse - 1]
+        valPassingCurrent = maxRobbingPreviousHouse
 
-        maxFromPreviousHouses[currentHouse] = max(valRobbingCurrent, valPassingCurrent)
+        curentMax = max(valRobbingCurrent, valPassingCurrent)
+
+        maxBeforePreviousHouse = maxRobbingPreviousHouse
+        maxRobbingPreviousHouse = curentMax
         currentHouse += 1
 
-    return maxFromPreviousHouses[len(nums) - 1]
+    return curentMax
 
 
 
+assert rob([7]) == 7
 assert rob([1,2,3,1]) == 4
 assert rob([2,7,9,3,1]) == 12
 assert rob([183,219,57,193,94,233,202,154,65,240,97,234,100,249,186,66,90,238,168,128,177,235,50,81,185,165,217,207,88,80,112,78,135,62,228,247,211]) == 3365
