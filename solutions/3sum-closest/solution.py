356aee36c92948c7e811854c33a304fadada88f3
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-05-04 20:48:16 -0500

    Updated "16. 3Sum Closest" to o(n^2)

--- solutions/3sum-closest/solution.py
+++ solutions/3sum-closest/solution.py
@@ -4,6 +4,26 @@ from itertools import combinations
 :type target: int
 :rtype: int
 """
+# Uses two pointers (secondIndex and thirdIndex) to close in on the target for each first number
+def threeSumClosest(nums, target): # o(n^2)
+    nums.sort()
+    closestToTarget = nums[0] + nums[1] + nums[2]
+    for firstIndex in range(len(nums) - 2):
+        secondIndex = firstIndex + 1
+        thirdIndex = len(nums) - 1
+        while secondIndex < thirdIndex:
+            currentSum = nums[firstIndex] + nums[secondIndex] + nums[thirdIndex]
+
+            if currentSum == target: return target
+            if abs(target - currentSum) < abs(target - closestToTarget): closestToTarget = currentSum
+
+            if currentSum < target: secondIndex += 1
+            else: thirdIndex -= 1
+
+    return closestToTarget
+
+
+#######################################################################################################
 # Uses itertools combinations to generate all combinations of 3 nums
 def threeSumClosestLame(nums, target): # o(cringe)
     closestToTarget = nums[0] + nums[1] + nums[2]
@@ -16,5 +36,5 @@ def threeSumClosestLame(nums, target): # o(cringe)
 
 
 
-assert threeSumClosestLame([-1,2,1,-4], 1) == 2
-assert threeSumClosestLame([0,0,0], 1) == 0
+assert threeSumClosest([-1,2,1,-4], 1) == 2
+assert threeSumClosest([0,0,0], 1) == 0
