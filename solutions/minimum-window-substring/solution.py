2e0ec7d16aa6b88881d3c0d36c66f9ff08a19b57
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-05-05 18:14:31 -0500

    Refactored "76. Minimum Window Substring"

--- solutions/minimum-window-substring/solution.py
+++ solutions/minimum-window-substring/solution.py
@@ -5,45 +5,24 @@ from collections import Counter
 :rtype: str
 """
 def minWindow(s, t):
-    charsNeeded = set(t)
+    charsNeeded = len(t)
     neededCharsCount = Counter(t)
     minWindowString = ""
 
     startIndex = 0
-    endIndex = 0
-    inSubstringWindow = False
-    growingWindow = True
-    while startIndex < len(s) and endIndex < len(s):
-        if not inSubstringWindow:
-            if s[startIndex] in charsNeeded: inSubstringWindow = True
-            else:
-                startIndex += 1
-                endIndex += 1
-        else: # in a potential substring window
-            if growingWindow: # grow window by walking endindex forward
-                if s[endIndex] in neededCharsCount: 
-                    neededCharsCount[s[endIndex]] -= 1
-                    if neededCharsCount[s[endIndex]] < 1 and s[endIndex] in charsNeeded: charsNeeded.remove(s[endIndex])
-                if not charsNeeded:
-                    if (minWindowString == "") != (len(minWindowString) > endIndex - startIndex): minWindowString = s[startIndex:endIndex + 1]
+    for endIndex in range(len(s)):
+        if s[endIndex] in neededCharsCount:
+            if neededCharsCount[s[endIndex]] > 0: charsNeeded -= 1
+            neededCharsCount[s[endIndex]] -= 1
+        
+        while not charsNeeded:
+            if (minWindowString == "") != (len(minWindowString) > endIndex - startIndex): minWindowString = s[startIndex:endIndex + 1]
 
-                    growingWindow = False
-                    neededCharsCount[s[startIndex]] += 1
-                    if neededCharsCount[s[startIndex]] > 0: charsNeeded.add(s[startIndex])
-                    startIndex += 1
-                else: endIndex += 1
-            else: # shrink window by walking startindex forward
-                if s[startIndex] in neededCharsCount:
-                    if not charsNeeded:
-                        if len(minWindowString) > endIndex - startIndex: minWindowString = s[startIndex:endIndex + 1]
-                        
-                        neededCharsCount[s[startIndex]] += 1
-                        if neededCharsCount[s[startIndex]] > 0: charsNeeded.add(s[startIndex])
-                        startIndex += 1
-                    else:
-                        growingWindow = True
-                        endIndex += 1
-                else: startIndex += 1
+            if s[startIndex] in neededCharsCount:
+                neededCharsCount[s[startIndex]] += 1
+                if neededCharsCount[s[startIndex]] > 0: charsNeeded += 1
+            startIndex += 1
+            
     return minWindowString
 
 
