281ba7eeb198c24b6f3978c7eadc28f8f2770c77
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-05-05 19:15:22 -0500

    Added iterative solution to "101. Symmetric Tree"

--- solutions/symmetric-tree/solution.py
+++ solutions/symmetric-tree/solution.py
@@ -8,6 +8,22 @@ class TreeNode(object):
 :type root: TreeNode
 :rtype: bool
 """
+def isSymmetric(root):
+    nodeStack = [(root.left, root.right)]
+    while nodeStack:
+        left, right = nodeStack.pop()
+
+        onlyOneNodeIsNone = (left == None) != (right == None)
+        if onlyOneNodeIsNone: return False
+
+        if left != None: # then right is also not None
+            if left.val != right.val: return False
+            nodeStack.append((left.left, right.right))
+            nodeStack.append((left.right, right.left))
+    return True
+
+
+###################################################################################################
 def isSymmetricRecursive(root):
     return twoNodesSymmetric(root.left, root.right)
 
