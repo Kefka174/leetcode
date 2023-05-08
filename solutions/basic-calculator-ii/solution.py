698fa357c97509d08ae30439634a5b8f0fed0b36
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-05-08 02:26:03 -0500

    Added Stack solution to "227. Basic Calculator II"

--- solutions/basic-calculator-ii/solution.py
+++ solutions/basic-calculator-ii/solution.py
@@ -2,7 +2,26 @@
 :type s: str
 :rtype: int
 """
-def calculate(s): # converts string into list of numbers and operators, takes 2 passes to process */ then +-
+def calculate(s):
+    additionSubtractionStack = []
+    currentNum = 0
+    previousOperator = '+'
+    for i in range(len(s)):
+        if s[i].isdigit(): currentNum = (currentNum * 10) + (ord(s[i]) - 48)
+        if (not s[i].isdigit() and s[i] != ' ') or i == len(s) - 1: # reached a new operator or end of s
+            if previousOperator == '-': additionSubtractionStack.append(-currentNum)
+            elif previousOperator == '+': additionSubtractionStack.append(currentNum)
+            elif previousOperator == '*': additionSubtractionStack.append(additionSubtractionStack.pop() * currentNum)
+            elif additionSubtractionStack[-1] > 0: additionSubtractionStack.append(additionSubtractionStack.pop() // currentNum)
+            else: additionSubtractionStack.append(-(additionSubtractionStack.pop() * -1 // currentNum)) # ceil instead of floor for integer division with negative number
+            previousOperator = s[i]
+            currentNum = 0
+
+    return sum(additionSubtractionStack)
+
+
+###################################################################################################################
+def calculateBulky(s): # converts string into list of numbers and operators, takes 2 passes to process */ then +-
     expression = []
     for char in s:
         if char != ' ':
@@ -35,3 +54,4 @@ assert calculate(" 3+5 / 2 ") == 5
 assert calculate("1-1+1") == 1
 assert calculate("0") == 0
 assert calculate("2*3+4") == 10
+assert calculate("14-3/2") == 13
