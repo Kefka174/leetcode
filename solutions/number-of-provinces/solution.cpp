6b96f6731e498eb16c99d54fa19282c782631e64
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-05-09 17:35:53 -0500

    Refactored "547. Number of Provinces"

--- solutions/number-of-provinces/solution.cpp
+++ solutions/number-of-provinces/solution.cpp
@@ -2,26 +2,30 @@
 #include <queue>
 using namespace std;
 
-int findCircleNum(vector<vector<int>>& isConnected) {
+int findCircleNumDFS(vector<vector<int>>& isConnected) { // O(n^2)
     int numProvinces = 0;
     vector<bool> visitedCities(isConnected.size());
-    queue<int> citiesToVisit;
 
     for (int i = 0; i < isConnected.size(); i++) {
         if (visitedCities[i] == false) {
             numProvinces++;
-            citiesToVisit.push(i);
-            while (!citiesToVisit.empty()) {
-                int currentCity = citiesToVisit.front();
-                citiesToVisit.pop();
-                for (int j = 0; j < isConnected[currentCity].size(); j++) {
-                    if (isConnected[currentCity][j] == 1 && visitedCities[j] == false) {
-                        visitedCities[j] = true;
-                        citiesToVisit.push(j);
-                    }
-                }
-            }
+            dfs(i, isConnected, visitedCities);
         }
     }
     return numProvinces;
+}
+
+void dfs(int startingNode, vector<vector<int>> graph, vector<bool>& visitedNodes) {
+    queue<int> citiesToVisit;
+    citiesToVisit.push(startingNode);
+    while (!citiesToVisit.empty()) {
+        int currentCity = citiesToVisit.front();
+        citiesToVisit.pop();
+        for (int i = 0; i < graph[currentCity].size(); i++) {
+            if (graph[currentCity][i] == 1 && visitedNodes[i] == false) {
+                visitedNodes[i] = true;
+                citiesToVisit.push(i);
+            }
+        }
+    }
 }
