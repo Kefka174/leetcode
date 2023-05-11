cd1e426393d0557be6c38ced4623789cb4e3f8ac
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-05-11 15:24:52 -0500

    Added iterative solution to "46. Permutations"

--- solutions/permutations/solution.cpp
+++ solutions/permutations/solution.cpp
@@ -1,8 +1,36 @@
 #include <vector>
 #include <algorithm> // copy_if
+#include <unordered_set>
+#include <queue>
 using namespace std;
 
-vector<vector<int>> permute(vector<int>& nums) {
+vector<vector<int>> permuteIterative(vector<int>& nums) {
+    vector<vector<int>> allPermutatoins;
+    unordered_set<int> numsSet (nums.begin(), nums.end());
+    queue<pair<vector<int>, unordered_set<int>>> permQueue;
+    permQueue.push({vector<int>(), numsSet});
+
+    while (!permQueue.empty()) {
+        vector<int> currentPermutation = permQueue.front().first;
+        unordered_set<int> numsToUse = permQueue.front().second;
+        permQueue.pop();
+
+        for (int num : numsToUse) {
+            unordered_set setWithoutNum = numsToUse;
+            setWithoutNum.erase(num);
+            vector<int> permWithNum = currentPermutation;
+            permWithNum.push_back(num);
+
+            if (setWithoutNum.empty()) allPermutatoins.push_back(permWithNum);
+            else permQueue.push({permWithNum, setWithoutNum});
+        }
+    }
+    return allPermutatoins;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+vector<vector<int>> permuteRecursive(vector<int>& nums) {
     if (nums.size() == 1) return vector<vector<int>>{{nums[0]}};
 
     vector<vector<int>> permutations;
@@ -13,7 +41,7 @@ vector<vector<int>> permute(vector<int>& nums) {
         copy_if(nums.begin(), nums.end(), numsWithoutI.begin(), [numI](int j){return j != numI;});
         
         // recursively generate permutations of nums without numI
-        vector<vector<int>> permsWithoutI = permute(numsWithoutI);
+        vector<vector<int>> permsWithoutI = permuteRecursive(numsWithoutI);
         for (vector<int>& perm : permsWithoutI) perm.push_back(numI); // add numI to each
         permutations.insert(permutations.end(), permsWithoutI.begin(), permsWithoutI.end());
     }
