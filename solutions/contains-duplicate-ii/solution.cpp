969e197157198d29c9ebb084e0a6f13c142dac23
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-05-16 17:02:52 -0500

    Updated "219. Contains Duplicate II" to use O(k) space

--- solutions/contains-duplicate-ii/solution.cpp
+++ solutions/contains-duplicate-ii/solution.cpp
@@ -1,12 +1,15 @@
 #include <vector>
-#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 bool containsNearbyDuplicate(vector<int>& nums, int k) {
-    unordered_map<int, int> numMap;
+    if (k == 0) return false;
+    unordered_set<int> numSet;
     for (int i = 0; i < nums.size(); i++) {
-        if (numMap.count(nums[i]) && i - numMap[nums[i]] <= k) return true;
-        numMap[nums[i]] = i;
+        if (numSet.count(nums[i])) return true;
+        
+        if (i >= k) numSet.erase(nums[i - k]);
+        numSet.insert(nums[i]);
     }
     return false;
 }
