8f93397d17250d539fd8c2f427a31e29b6d7111a
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-05-19 13:39:14 -0500

    Added manual solution to "338. Counting Bits"

--- solutions/counting-bits/solution.cpp
+++ solutions/counting-bits/solution.cpp
@@ -1,8 +1,26 @@
 #include <vector>
 #include <bitset>
+#include <cmath> // pow(), log2()
 using namespace std;
 
-vector<int> countBits(int n) {
+vector<int> countBitsManual(int n) { // O(n*logn)
+    vector<int> bitCounts {0};
+    for (int i = 1; i < n + 1; i++) {
+        int iVal = i;
+        int iBitCount = 0;
+        for (int bit = ceil(log2(i)) + 1; bit >= 0 && iVal != 0; bit--) {
+            if (iVal >= pow(2, bit)) {
+                iVal -= pow(2, bit);
+                iBitCount++;
+            }
+        }
+        bitCounts.push_back(iBitCount);
+    }
+    return bitCounts;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+vector<int> countBitsBuiltIn(int n) {
     vector<int> bitCounts;
     for (int i = 0; i < n + 1; i++) bitCounts.push_back(bitset<32>(i).count());
     return bitCounts;
