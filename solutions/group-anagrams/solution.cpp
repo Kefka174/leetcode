e2891899958adcb8efed6a2e3dfae42baa9a058d
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-05-20 16:05:38 -0500

    Added sorting solution to "49. Group Anagrams"

--- solutions/group-anagrams/solution.cpp
+++ solutions/group-anagrams/solution.cpp
@@ -1,8 +1,28 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
+vector<vector<string>> groupAnagramsSorting(vector<string>& strs) {
+    vector<vector<string>> anagrams;
+    unordered_map<string, int> strGroups;
+
+    for (string s : strs) {
+        string sortedS = s;
+        sort(sortedS.begin(), sortedS.end());
+        
+        if (strGroups.count(sortedS)) anagrams[strGroups[sortedS]].push_back(s);
+        else {
+            strGroups[sortedS] = anagrams.size();
+            anagrams.push_back(vector<string> {s});
+        }
+    }
+    return anagrams;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
 vector<vector<string>> groupAnagrams(vector<string>& strs) {
     vector<vector<string>> anagrams;
     vector<unordered_map<char, int>> strGroups;
