5903db3fe017a3157d9bbf610098c59e9725444e
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-06-01 16:50:48 -0500

    Updated "3Sum" to O(unique n) space

--- solutions/3sum/solution.cpp
+++ solutions/3sum/solution.cpp
@@ -1,44 +1,31 @@
 #include <vector>
 #include <unordered_map>
-#include <unordered_set>
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
-struct hashFunction { // vector<int> hashing
-  size_t operator()(const vector<int>& vect) const {
-    std::size_t seed = vect.size();
-    for(auto& i : vect) {
-        seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-    }
-    return seed;
-  }
-};
+vector<vector<int>> threeSumHashMap(vector<int>& nums) { // O(n^2) time, O(unique n) space
+    sort(nums.begin(), nums.end());
 
-vector<vector<int>> threeSumBrute(vector<int>& nums) { // O(n^2) time and space
-    unordered_map<int, vector<pair<int, int>>> twoSums;
-    for (int i = 0; i < nums.size(); i++) {
-        for (int j = i + 1; j < nums.size(); j++) {
-            int sum = nums[i] + nums[j];
-            pair<int, int> indexPair = {i, j};
-            if (nums[i] > nums[j]) indexPair = {j, i}; // keep values sorted increasing
+    unordered_map<int, int> finalIndexOfVal;
+    for (int i = 0; i < nums.size(); i++) finalIndexOfVal[nums[i]] = i;
 
-            if (twoSums.count(sum)) twoSums[sum].push_back(indexPair);
-            else twoSums.insert({sum, {indexPair}});
-        }
-    }
+    vector<vector<int>> validTriplets;
+    for (int firstIndex = 0; firstIndex < nums.size() && nums[firstIndex] <= 0; firstIndex++) {
+        while (firstIndex != 0 && firstIndex < nums.size() - 1 
+                && nums[firstIndex] == nums[firstIndex-1]) firstIndex++; // pass over duplicate first values
 
-    unordered_set<vector<int>, hashFunction> validTriplets; // set so there aren't duplicates
-    for (int i = 0; i < nums.size(); i++) {
-        for (pair<int, int> indexPair : twoSums[-nums[i]]) {
-            if (i != indexPair.first && i != indexPair.second) {
-                // insert in sorted order
-                if (nums[i] < nums[indexPair.first]) validTriplets.insert({nums[i], nums[indexPair.first], nums[indexPair.second]});
-                else if (nums[i] < nums[indexPair.second]) validTriplets.insert({nums[indexPair.first], nums[i], nums[indexPair.second]});
-                else validTriplets.insert({nums[indexPair.first], nums[indexPair.second], nums[i]});
+        for (int secondIndex = firstIndex + 1; secondIndex < nums.size(); secondIndex++) {
+            while (secondIndex != firstIndex + 1 && secondIndex < nums.size() - 1 
+                    && nums[secondIndex] == nums[secondIndex-1]) secondIndex++; // pass over duplicate second values
+
+            int valNeeded = -(nums[firstIndex] + nums[secondIndex]);
+            if (finalIndexOfVal.count(valNeeded) && finalIndexOfVal[valNeeded] > secondIndex) {
+                validTriplets.push_back({nums[firstIndex], nums[secondIndex], valNeeded});
             }
         }
     }
-    return vector<vector<int>> (validTriplets.begin(), validTriplets.end());
+    return validTriplets;
 }
 
 
@@ -48,14 +35,14 @@ void printVector2D(vector<vector<int>>& vect) {
     for (vector<int> nums : vect) {
         cout << "< ";
         for (int num : nums) cout << num << ", ";
-        cout << "> ";
+        cout << ">, ";
     }
     cout << ">" << endl;
 }
 
 int main() {
     vector<int> v1{-1,0,1,2,-1,-4};
-    vector<vector<int>> r1 = threeSumBrute(v1);
+    vector<vector<int>> r1 = threeSumHashMap(v1);
     printVector2D(r1);
 
     return 0;
