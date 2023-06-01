0e2ad252c1ca645b0b1702e2289a00ebc5628379
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-06-01 18:22:02 -0500

    Added two-pointer solution to "3Sum"

--- solutions/3sum/solution.cpp
+++ solutions/3sum/solution.cpp
@@ -4,6 +4,37 @@
 #include <iostream>
 using namespace std;
 
+vector<vector<int>> threeSumTwoPointer(vector<int>& nums) { // O(n^2) time
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> validTriplets;
+    for (int firstIndex = 0; firstIndex < nums.size() - 2 && nums[firstIndex] <= 0; firstIndex++) {
+        while (firstIndex != 0 && firstIndex < nums.size() - 2 
+                && nums[firstIndex] == nums[firstIndex-1]) firstIndex++; // pass over duplicate first values
+        
+        int secondIndex = firstIndex + 1;
+        int thirdIndex = nums.size() - 1;
+        while (secondIndex < thirdIndex) {
+            int sum = nums[firstIndex] + nums[secondIndex] + nums[thirdIndex];
+            if (sum == 0) {
+                validTriplets.push_back({nums[firstIndex], nums[secondIndex], nums[thirdIndex]});
+                secondIndex++;
+                thirdIndex--;
+            }
+            else if (sum < 0) secondIndex++;
+            else thirdIndex--;
+
+            // pass over duplicate second and third values
+            while (secondIndex != firstIndex + 1 && secondIndex < thirdIndex 
+                    && nums[secondIndex] == nums[secondIndex - 1]) secondIndex++;
+            while (thirdIndex != nums.size() - 1 && thirdIndex > secondIndex
+                    && nums[thirdIndex] == nums[thirdIndex + 1]) thirdIndex--;
+        }
+    }
+    return validTriplets;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 vector<vector<int>> threeSumHashMap(vector<int>& nums) { // O(n^2) time, O(unique n) space
     sort(nums.begin(), nums.end());
 
@@ -11,13 +42,13 @@ vector<vector<int>> threeSumHashMap(vector<int>& nums) { // O(n^2) time, O(uniqu
     for (int i = 0; i < nums.size(); i++) finalIndexOfVal[nums[i]] = i;
 
     vector<vector<int>> validTriplets;
-    for (int firstIndex = 0; firstIndex < nums.size() && nums[firstIndex] <= 0; firstIndex++) {
-        while (firstIndex != 0 && firstIndex < nums.size() - 1 
-                && nums[firstIndex] == nums[firstIndex-1]) firstIndex++; // pass over duplicate first values
+    for (int firstIndex = 0; firstIndex < nums.size() - 2 && nums[firstIndex] <= 0; firstIndex++) {
+        while (firstIndex != 0 && firstIndex < nums.size() - 2 
+                && nums[firstIndex] == nums[firstIndex - 1]) firstIndex++; // pass over duplicate first values
 
-        for (int secondIndex = firstIndex + 1; secondIndex < nums.size(); secondIndex++) {
+        for (int secondIndex = firstIndex + 1; secondIndex < nums.size() - 1; secondIndex++) {
             while (secondIndex != firstIndex + 1 && secondIndex < nums.size() - 1 
-                    && nums[secondIndex] == nums[secondIndex-1]) secondIndex++; // pass over duplicate second values
+                    && nums[secondIndex] == nums[secondIndex - 1]) secondIndex++; // pass over duplicate second values
 
             int valNeeded = -(nums[firstIndex] + nums[secondIndex]);
             if (finalIndexOfVal.count(valNeeded) && finalIndexOfVal[valNeeded] > secondIndex) {
