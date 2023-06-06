900e3f31e55cbe7f5d8db71ddf3876a96b77c547
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-06-06 09:39:43 -0500

    Refactored "53. Maximum Subarray"

--- solutions/maximum-subarray/solution.cpp
+++ solutions/maximum-subarray/solution.cpp
@@ -3,22 +3,11 @@ using namespace std;
 
 int maxSubArray(vector<int>& nums) {
     int maxTotal = nums[0];
-    bool inSubArray = false;
-    int subArrayTotal;
+    int subArrayTotal = 0;
 
     for (int num : nums) {
-        if (inSubArray) {
-            subArrayTotal += num;
-            if (subArrayTotal > maxTotal) maxTotal = subArrayTotal;
-            if (subArrayTotal < 0) inSubArray = false;
-        }
-        else {
-            if (num > maxTotal) maxTotal = num;
-            if (num > 0) {
-                inSubArray = true;
-                subArrayTotal = num;
-            }
-        }
+        subArrayTotal = max(num, subArrayTotal + num);
+        maxTotal = max(maxTotal, subArrayTotal);
     }
     return maxTotal;
 }
