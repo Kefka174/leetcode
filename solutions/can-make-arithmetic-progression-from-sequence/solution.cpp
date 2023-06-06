07026f66832f55b09945bb10b08a5fe07d7b835a
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-06-06 12:01:02 -0500

    Updated "1502. Can Make Arithmetic Progression From Sequence" to O(n) time and space

--- solutions/can-make-arithmetic-progression-from-sequence/solution.cpp
+++ solutions/can-make-arithmetic-progression-from-sequence/solution.cpp
@@ -1,8 +1,31 @@
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
 using namespace std;
 
-bool canMakeArithmeticProgression(vector<int>& arr) { // O(nlogn)
+bool canMakeArithmeticProgression(vector<int>& arr) { // O(n) time and space
+    int minVal = arr[0];
+    int maxVal = arr[0];
+    for (int i = 1; i < arr.size(); i++) {
+        minVal = min(minVal, arr[i]);
+        maxVal = max(maxVal, arr[i]);
+    }
+
+    if ((maxVal - minVal) % (arr.size() - 1) != 0) return false;
+    int minDiff = (maxVal - minVal) / (arr.size() - 1);
+    if (minDiff == 0) return true;
+
+    unordered_set<int> diffsUsed; // can be done with O(1) space by swapping values in the original
+    for (int i = 0; i < arr.size(); i++) {
+        int currentDiff = arr[i] - minVal;
+        if (currentDiff % minDiff != 0 || diffsUsed.count(currentDiff)) return false;
+        diffsUsed.insert(currentDiff);
+    }
+    return true;
+}
+
+/////////////////////////////////////////////////////////////////
+bool canMakeArithmeticProgressionSort(vector<int>& arr) { // O(nlogn)
     sort(arr.begin(), arr.end());
     int diff = arr[1] - arr[0];
     for (int i = 2; i < arr.size(); i++) {
