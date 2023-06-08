91c2a7c5b039ef849aee1097eaae76e850af459b
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-06-07 19:45:05 -0500

    Updated "1351. Count Negative Numbers in a Sorted Matrix" to O(n + m)

--- solutions/count-negative-numbers-in-a-sorted-matrix/solution.cpp
+++ solutions/count-negative-numbers-in-a-sorted-matrix/solution.cpp
@@ -1,7 +1,22 @@
 #include <vector>
 using namespace std;
 
-int countNegatives(vector<vector<int>>& grid) { // O(n * m)
+int countNegatives(vector<vector<int>>& grid) { // O(n + m)
+    int row = grid.size() - 1;
+    int col = 0;
+    int negativeCount = 0;
+    while (row >= 0 && col < grid[0].size()) {
+        if (grid[row][col] < 0) {
+            row--;
+            negativeCount += grid[0].size() - col;
+        }
+        else col++;
+    }
+    return negativeCount;
+}
+
+////////////////////////////////////////////////////////////////
+int countNegativesBrute(vector<vector<int>>& grid) { // O(n * m)
     int negativeCount = grid.size() * grid[0].size();
     for (vector<int> row : grid) {
         int numIndex = 0;
