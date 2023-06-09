dba4b328d3d15916efe26b058986d3e95034d0fc
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-06-08 22:55:32 -0500

    Updated iterative solution for "105. Construct Binary Tree from Preorder and Inorder Traversal" to O(n)

--- solutions/construct-binary-tree-from-preorder-and-inorder-traversal/solution.cpp
+++ solutions/construct-binary-tree-from-preorder-and-inorder-traversal/solution.cpp
@@ -1,5 +1,7 @@
 #include <vector>
 #include <map>
+#include <stack>
+#include <tuple>
 #include <unordered_map>
 using namespace std;
 
@@ -12,11 +14,52 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+
+// Iterative implementation of the recursive solution below
+TreeNode* buildTreeIterative(vector<int>& preorder, vector<int>& inorder) { // O(n)
+    unordered_map<int, int> valInorderIndexes;
+    for (int i = 0; i < inorder.size(); i++) valInorderIndexes[inorder[i]] = i;
+
+    TreeNode *nodeParent, *head = nullptr;
+    int inorderStartIndex = 0;
+    int inorderEndIndex = inorder.size() - 1;
+    vector<int>::iterator preorderIterator = preorder.begin()++;
+    stack<tuple<TreeNode*, int, int>> rightChildrenToProcess;
+    while (inorderStartIndex <= inorderEndIndex || !rightChildrenToProcess.empty()) {
+        TreeNode *currentNode = new TreeNode(*preorderIterator);
+        int currentInorderIndex = valInorderIndexes[*preorderIterator];
+        preorderIterator++;
+
+        if (head == nullptr) head = currentNode;
+        else { // need to attach to parent
+            if (inorderStartIndex > inorderEndIndex) {
+                tie(nodeParent, inorderStartIndex, inorderEndIndex) = rightChildrenToProcess.top();
+                rightChildrenToProcess.pop();
+                nodeParent->right = currentNode;
+            }
+            else nodeParent->left = currentNode;
+        }
+
+        if (currentInorderIndex < inorderEndIndex) { // currentNode has right children
+            rightChildrenToProcess.push({currentNode, currentInorderIndex + 1, inorderEndIndex});
+        }
+
+        nodeParent = currentNode;
+        inorderEndIndex = currentInorderIndex - 1;
+    }
+
+    return head;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Creates nodes in the order of the preorder traversal. When a node is created, the inorder traversal list is split at that node.
+// The left part of the split inorder list become that node's left children, the right part of the split list become its right children.
 TreeNode* buildTreeRecursive(vector<int>& preorder, vector<int>& inorder) { // O(n)
     unordered_map<int, int> valInorderIndexes;
     for (int i = 0; i < inorder.size(); i++) valInorderIndexes[inorder[i]] = i;
-    
     vector<int>::iterator preorderIterator = preorder.begin();
+
     return recursiveHelper(preorderIterator, valInorderIndexes, 0, inorder.size() - 1);
 }
 
@@ -36,7 +79,8 @@ TreeNode* recursiveHelper(vector<int>::iterator& preorderIterator, unordered_map
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Creates nodes in the order of the preorder traversal, uses the inorder traversal to find the correct parent of each node
+// Creates nodes in the order of the preorder traversal and stores them in a map according to their inorder traversal ordering.
+// When a node is created, its parent is looked up in the map (takes logn time) and is connected to the new node.
 TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {// O(nlogn)
     unordered_map<int, int> valInorderIndexes;
     for (int i = 0; i < inorder.size(); i++) valInorderIndexes[inorder[i]] = i;
@@ -63,6 +107,7 @@ TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {// O(nlogn)
 
     return head;
 }
+
 
 // test cases: preorder, inorder
 // [1,2,3], [2,3,1]
