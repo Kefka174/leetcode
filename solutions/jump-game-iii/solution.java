b6283d4fe7f87c095657d793ecd3a83e2653ad26
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-06-14 19:42:33 -0500

    Added recursive solution to "1306. Jump Game III"

--- solutions/jump-game-iii/solution.java
+++ solutions/jump-game-iii/solution.java
@@ -4,6 +4,24 @@ import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
+    public boolean canReachRecursive(int[] arr, int start) {
+        if (arr[start] == 0) return true;
+        
+        int leftJumpIndex = start + arr[start];
+        int rightJumpIndex = start - arr[start];
+        arr[start] *= -1;
+
+        boolean leftReaches = false, rightReaches = false;
+        if (leftJumpIndex < arr.length && leftJumpIndex >= 0 && arr[leftJumpIndex] >= 0) 
+            leftReaches = canReachRecursive(arr, leftJumpIndex);
+        if (rightJumpIndex < arr.length && rightJumpIndex >= 0 && arr[rightJumpIndex] >= 0) 
+            rightReaches = canReachRecursive(arr, rightJumpIndex);
+
+        return leftReaches || rightReaches;
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public boolean canReach(int[] arr, int start) { // O(n) dfs
         Set<Integer> visitedIndices = new HashSet<>();
         Deque<Integer> indiciesToVisit = new ArrayDeque<>();
@@ -13,7 +31,7 @@ class Solution {
         while (!indiciesToVisit.isEmpty()) {
             int currentIndex = indiciesToVisit.pop();
             if (arr[currentIndex] == 0) return true;
-            
+
             checkIndexAndAddToStack(currentIndex + arr[currentIndex], arr.length, visitedIndices, indiciesToVisit);
             checkIndexAndAddToStack(currentIndex - arr[currentIndex], arr.length, visitedIndices, indiciesToVisit);
         }
