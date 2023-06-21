4e922c8d03b195f78ba1a0fdd8b9e4044dcb3611
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-06-21 16:37:14 -0500

    Refactored "1345. Jump Game IV" to actually be O(n)

--- solutions/jump-game-iv/solution.java
+++ solutions/jump-game-iv/solution.java
@@ -5,31 +5,36 @@ import java.util.Deque;
 import javafx.util.Pair;
 
 class Solution {
-    public int minJumpsBFS(int[] arr) { // times out
+    public int minJumpsBFS(int[] arr) { // O(n)
         Map<Integer, List<Integer>> valueIndicies = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
             if (!valueIndicies.containsKey(arr[i])) valueIndicies.put(arr[i], new ArrayList<>());
             valueIndicies.get(arr[i]).add(i);
         }
-        Set<Integer> visitedIndicesSet = new HashSet<>();
-        visitedIndicesSet.add(0);
-        Deque<Pair<Integer, Integer>> indiciesToVisitQueue = new ArrayDeque<>(); // pair of index, jumpCount
-        indiciesToVisitQueue.add(new Pair(0, 0));
+        Set<Integer> visitedValues = new HashSet<>();
+        Set<Integer> visitedIndices = new HashSet<>();
+        visitedIndices.add(0);
+        Deque<Pair<Integer, Integer>> indiciesToVisit = new ArrayDeque<>(); // pair of index, jumpCount
+        indiciesToVisit.add(new Pair(0, 0));
 
-        while (!indiciesToVisitQueue.isEmpty()) {
-            int currentIndex = indiciesToVisitQueue.peekFirst().getKey();
-            int jumpCount = indiciesToVisitQueue.peekFirst().getValue();
-            indiciesToVisitQueue.remove();
+        while (!indiciesToVisit.isEmpty()) {
+            int currentIndex = indiciesToVisit.peekFirst().getKey();
+            int jumpCount = indiciesToVisit.peekFirst().getValue();
+            indiciesToVisit.remove();
 
             if (currentIndex == arr.length - 1) return jumpCount;
+
             // jump backwards and forwards
-            addToSetAndQueue(currentIndex - 1, jumpCount + 1, visitedIndicesSet, indiciesToVisitQueue);
-            addToSetAndQueue(currentIndex + 1, jumpCount + 1, visitedIndicesSet, indiciesToVisitQueue);
+            addToSetAndQueue(currentIndex - 1, jumpCount + 1, visitedIndices, indiciesToVisit);
+            addToSetAndQueue(currentIndex + 1, jumpCount + 1, visitedIndices, indiciesToVisit);
             // jump to similar vals
-            for (int indexWithCurrentVal : valueIndicies.get(arr[currentIndex])) {
-                if (indexWithCurrentVal != currentIndex) {
-                    addToSetAndQueue(indexWithCurrentVal, jumpCount + 1, visitedIndicesSet, indiciesToVisitQueue);
+            if (!visitedValues.contains(arr[currentIndex])) {
+                for (int indexWithCurrentVal : valueIndicies.get(arr[currentIndex])) {
+                    if (indexWithCurrentVal != currentIndex) {
+                        addToSetAndQueue(indexWithCurrentVal, jumpCount + 1, visitedIndices, indiciesToVisit);
+                    }
                 }
+                visitedValues.add(arr[currentIndex]);
             }
         }
         return arr.length - 1; // unreachable
