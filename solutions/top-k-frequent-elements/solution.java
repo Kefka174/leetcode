be984f8495a4002f04e143e28d0b0c9828c9d687
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-06-24 17:03:57 -0500

    Added bucketsort solution to "347. Top K Frequent Elements"

--- solutions/top-k-frequent-elements/solution.java
+++ solutions/top-k-frequent-elements/solution.java
@@ -1,16 +1,46 @@
 import java.util.Map;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Arrays;
 
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) { // O(n*logn)
+    public int[] topKFrequentBucket(int[] nums, int k) { // O(n)
+        int maxFrequency = 0;
+        Map<Integer, Integer> frequencyCounter = new HashMap<>();
+        for (int num : nums) {
+            frequencyCounter.merge(num, 1, Integer::sum);
+            if (frequencyCounter.get(num) > maxFrequency) maxFrequency = frequencyCounter.get(num);
+        }
+
+        List<Integer>[] frequencyBuckets = new ArrayList[maxFrequency];
+        for (int num : frequencyCounter.keySet()) {
+            int placement = maxFrequency - frequencyCounter.get(num);
+            if (frequencyBuckets[placement] == null) frequencyBuckets[placement] = new ArrayList<>();
+            frequencyBuckets[placement].add(num);
+        }
+
+        int[] kFrequent = new int[k];
+        int arrIndex = 0, frequencyIndex = 0;
+        while (arrIndex < k) {
+            while (frequencyBuckets[frequencyIndex] == null) frequencyIndex++;
+            for (int i = 0; arrIndex < k && i < frequencyBuckets[frequencyIndex].size(); i++) {
+                kFrequent[arrIndex] = frequencyBuckets[frequencyIndex].get(i);
+                arrIndex++;
+            }
+            frequencyIndex++;
+        }
+        return kFrequent;
+    }
+
+
+    public int[] topKFrequentSorted(int[] nums, int k) { // O(n*logn)
         Map<Integer, Integer> frequencyCounter = new HashMap<>();
         for (int num : nums) frequencyCounter.merge(num, 1, Integer::sum);
 
-        int[] sortedByFrequency = frequencyCounter.entrySet().stream()
+        int[] frequencyBuckets = frequencyCounter.entrySet().stream()
                                                   .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                                                   .mapToInt(Map.Entry::getKey)
                                                   .toArray();
-        return Arrays.copyOfRange(sortedByFrequency, 0, k);
+        return Arrays.copyOfRange(frequencyBuckets, 0, k);
     }
 }
