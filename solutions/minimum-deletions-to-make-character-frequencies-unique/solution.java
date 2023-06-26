df0075d98935d7e6293d099d7d89b1d930548812
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-06-25 19:33:16 -0500

    Refactored "1647. Minimum Deletions to Make Character Frequencies Unique"

--- solutions/minimum-deletions-to-make-character-frequencies-unique/solution.java
+++ solutions/minimum-deletions-to-make-character-frequencies-unique/solution.java
@@ -1,50 +1,33 @@
+import java.util.Collection;
 import java.util.Collections;
+import java.util.stream.Collectors;
 import java.util.Map;
-import java.util.Set;
+import java.util.List;
 
 class Solution {
     public int minDeletions(String s) {
-        Map<Character, Integer> charFrequencies = countFrequencies(s);
-        int maxFrequency = Collections.max(charFrequencies.values());
-
-        Map<Integer, Set<Character>> frequencyBuckets = frequencyMapToBuckets(charFrequencies);
-
+        List<Character> charList = s.chars().mapToObj(e->(char)e).collect(Collectors.toList());
+        Map<Character, Integer> charFrequencies = makeFrequencyMap(charList);
+        
+        Map<Integer, Integer> frequencyCounts = makeFrequencyMap(charFrequencies.values());
+        
         int deletionCount = 0;
+        int maxFrequency = Collections.max(frequencyCounts.keySet());
         for (int frequency = maxFrequency; frequency > 0; frequency--) {
-            Set<Character> charsWithFrequency = frequencyBuckets.get(frequency);
-            if (charsWithFrequency != null && charsWithFrequency.size() > 1) {
-                deletionCount += charsWithFrequency.size() - 1;
-                makeFrequencyUnique(frequency, frequencyBuckets);
+            Integer charsWithFrequency = frequencyCounts.get(frequency);
+            if (charsWithFrequency != null && charsWithFrequency > 1) {
+                deletionCount += charsWithFrequency - 1;
+                frequencyCounts.merge(frequency - 1, charsWithFrequency - 1, Integer::sum);
             }
         }
         return deletionCount;
     }
 
-    private Map<Character, Integer> countFrequencies(String s) {
-        Map<Character, Integer> charFrequencies = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            charFrequencies.merge(s.charAt(i), 1, Integer::sum);
+    private <T> Map<T, Integer> makeFrequencyMap(Collection<T> items) {
+        Map<T, Integer> frequencies = new HashMap<>();
+        for (T item : items) {
+            frequencies.merge(item, 1, Integer::sum);
         }
-        return charFrequencies;
-    }
-
-    private Map<Integer, Set<Character>> frequencyMapToBuckets(Map<Character, Integer> charFrequencies) {
-        Map<Integer, Set<Character>> frequencyBuckets = new HashMap<>();
-        for (char c : charFrequencies.keySet()) {
-            int frequency = charFrequencies.get(c);
-            if (!frequencyBuckets.containsKey(frequency)) frequencyBuckets.put(frequency, new HashSet<>());
-            frequencyBuckets.get(frequency).add(c);
-        }
-        return frequencyBuckets;
-    }
-
-    private void makeFrequencyUnique(int frequency, Map<Integer, Set<Character>> frequencyBuckets) {
-        Set<Character> charsWithFrequency = frequencyBuckets.get(frequency);
-        if (!frequencyBuckets.containsKey(frequency - 1)) frequencyBuckets.put(frequency - 1, new HashSet<>());
-        while (charsWithFrequency.size() > 1) {
-            char c = charsWithFrequency.iterator().next();
-            charsWithFrequency.remove(c);
-            frequencyBuckets.get(frequency - 1).add(c);
-        }
+        return frequencies;
     }
 }
