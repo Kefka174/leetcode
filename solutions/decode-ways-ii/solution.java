78d266b510ebd76107f153aa9da1b934603bebb3
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-06-30 21:41:56 -0500

    Refactored "639. Decode Ways II"

--- solutions/decode-ways-ii/solution.java
+++ solutions/decode-ways-ii/solution.java
@@ -1,5 +1,6 @@
 class Solution {
     public int numDecodings(String s) {
+        int M = 1000000007;
         long currentCount = 1, prevCount = 1, prevPrevCount = 1;
         for (int i = s.length() - 1; i >= 0; i--) {
             if (s.charAt(i) == '0') {
@@ -12,34 +13,35 @@ class Solution {
                 i--;
             }
             else {
-                boolean canBeSingle = (i == s.length() - 1 || s.charAt(i + 1) != '0');
-                boolean canBeFirstOfPair = (i < s.length() - 1 && s.charAt(i) != '0') && 
-                                        (i + 2 == s.length() || s.charAt(i + 2) != '0');
-                if (canBeSingle) {
-                    if (s.charAt(i) == '*') currentCount *= 9;
-                }
+                boolean canBeSingleDigit = (i == s.length() - 1 || s.charAt(i + 1) != '0');
+                boolean canBeFirstOfPair = digitCanBeFirstOfPair(s, i);
+
+                if (canBeSingleDigit && s.charAt(i) == '*') currentCount *= 9;
                 if (canBeFirstOfPair) {
-                    switch (s.charAt(i)) {
-                        case '1':
-                            if (s.charAt(i + 1) == '*') currentCount += 9 * prevPrevCount;
-                            else currentCount += prevPrevCount;
-                            break;
-                        case '2':
-                            if (s.charAt(i + 1) == '*') currentCount += 6 * prevPrevCount;
-                            else if (s.charAt(i + 1) <= '6') currentCount += prevPrevCount;
-                            break;
-                        case '*':
-                            if (s.charAt(i + 1) == '*') currentCount += 15 * prevPrevCount;
-                            else if (s.charAt(i + 1) > '6') currentCount += prevPrevCount;
-                            else currentCount += 2 * prevPrevCount;
-                            break;
-                    }
+                    if (s.charAt(i) == '1' && s.charAt(i + 1) == '*') 
+                        currentCount += 9 * prevPrevCount;
+                    else if (s.charAt(i) == '2' && s.charAt(i + 1) == '*') 
+                        currentCount += 6 * prevPrevCount;
+                    else if (s.charAt(i) == '*' && s.charAt(i + 1) == '*') 
+                        currentCount += 15 * prevPrevCount;
+                    else if (s.charAt(i) == '*' && s.charAt(i + 1) <= '6') 
+                        currentCount += 2 * prevPrevCount;
+                    else currentCount += prevPrevCount;
                 }
-                prevPrevCount = prevCount % 1000000007;
-                prevCount = currentCount % 1000000007;
-                currentCount = currentCount % 1000000007;
+                // problem asks for the answer to be modulo (10^9 + 7) due to memory limitations
+                prevPrevCount = prevCount % M;
+                prevCount = currentCount % M;
+                currentCount = currentCount % M;
             }
         }
         return (int)currentCount;
+    }
+
+    private boolean digitCanBeFirstOfPair(String s, int index) {
+        if (index == s.length() - 1) return false;
+        boolean digitIsLessThan26 = (s.charAt(index) <= '2') && 
+                                    (s.charAt(index) != '2' || s.charAt(index + 1) <= '6');
+        boolean nextDigitCanBeInPair = (index + 2 == s.length() || s.charAt(index + 2) != '0');
+        return digitIsLessThan26 && nextDigitCanBeInPair;
     }
 }
