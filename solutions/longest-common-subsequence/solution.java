ad6fc7d875925cec719683e5465be8931d56c68f
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-07-03 16:38:52 -0500

    Refactored "1143. Longest Common Subsequence"

--- solutions/longest-common-subsequence/solution.java
+++ solutions/longest-common-subsequence/solution.java
@@ -2,25 +2,26 @@ import java.util.Map;
 import javafx.util.Pair;
 
 class Solution {
-    public int longestCommonSubsequenceBrute(String text1, String text2) {
+    public int longestCommonSubsequence(String text1, String text2) {
         Map<Pair<Integer, Integer>, Integer> memo = new HashMap<>();
         return recursiveHelper(text1, text2, 0, 0, memo);
     }
 
     private int recursiveHelper(String s1, String s2, int index1, int index2, Map<Pair<Integer, Integer>, Integer> memo) {
-        if (index1 >= s1.length() || index2 >= s2.length()) return 0;
+        if (index1 == s1.length() || index2 == s2.length()) return 0;
         Pair<Integer, Integer> memoKey = new Pair(index1, index2);
         if (memo.containsKey(memoKey)) return memo.get(memoKey);
 
-        int totalSkippingCurrentChar = 0;
-        totalSkippingCurrentChar = recursiveHelper(s1, s2, index1 + 1, index2, memo);
-        
-        int totalUsingCurrentChar = 0;
-        while (index2 < s2.length() && s1.charAt(index1) != s2.charAt(index2)) index2++;
-        if (index2 < s2.length()) totalUsingCurrentChar++;
-        totalUsingCurrentChar += recursiveHelper(s1, s2, index1 + 1, index2 + 1, memo);
+        int total = 0;
+        if (s1.charAt(index1) == s2.charAt(index2)) 
+            total = 1 + recursiveHelper(s1, s2, index1 + 1, index2 + 1, memo);
+        else {
+            int skip1 = recursiveHelper(s1, s2, index1 + 1, index2, memo);
+            int skip2 = recursiveHelper(s1, s2, index1, index2 + 1, memo);
+            total = Math.max(skip1, skip2);
+        }
 
-        memo.put(memoKey, Math.max(totalSkippingCurrentChar, totalUsingCurrentChar));
-        return memo.get(memoKey);
+        memo.put(memoKey, total);
+        return total;
     }
 }
