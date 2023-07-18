73da4993dadbd88eb18e28825859d225d6dd24e0
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-07-18 05:08:57 -0500

    Updated "11. Container With Most Water" to O(n)

--- solutions/container-with-most-water/solution.java
+++ solutions/container-with-most-water/solution.java
@@ -1,4 +1,21 @@
 class Solution {
+    public int maxAreaTwoPointer(int[] height) { // O(n)
+        int maximumArea = 0;
+        int startIndex = 0;
+        int endIndex = height.length - 1;
+        while (startIndex < endIndex) {
+            int area = Math.min(height[startIndex], height[endIndex]) * (endIndex - startIndex);
+            if (area > maximumArea) maximumArea = area;
+            
+            if (height[startIndex] < height[endIndex]) startIndex++;
+            else endIndex--;
+        }
+
+        return maximumArea;
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
     public int maxAreaBrute(int[] height) { // O(n^2)
         int maximumArea = 0;
         for (int i = 0; i < height.length; i++) {
