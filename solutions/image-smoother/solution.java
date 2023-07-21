9822462aad477343ae18cb73ab96f6a908d1c1a0
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-07-21 04:14:33 -0500

    Refactored "661. Image Smoother"

--- solutions/image-smoother/solution.java
+++ solutions/image-smoother/solution.java
@@ -1,6 +1,28 @@
 import java.util.List;
 
 class Solution {
+    public int[][] imageSmootherCondensed(int[][] img) {
+        int[][] smoothedImage = new int[img.length][img[0].length];
+        for (int row = 0; row < img.length; row++) {
+            for (int col = 0; col < img[0].length; col++) {
+                int sum = 0, count = 0;
+                for (int neighborRow = row - 1; neighborRow <= row + 1; neighborRow++) {
+                    for (int neighborCol = col - 1; neighborCol <= col + 1; neighborCol++) {
+                        if (neighborRow >= 0 && neighborRow != img.length &&
+                            neighborCol >= 0 && neighborCol != img[0].length) {
+                                sum += img[neighborRow][neighborCol];
+                                count++;
+                        }
+                    }
+                }
+                smoothedImage[row][col] = sum / count;
+            }
+        }
+        return smoothedImage;
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////
     public int[][] imageSmoother(int[][] img) {
         int[][] smoothedImage = new int[img.length][img[0].length];
         for (int row = 0; row < img.length; row++) {
@@ -17,29 +39,12 @@ class Solution {
 
     private List<Integer> getNeighborValues(int row, int col, int[][] img) {
         List<Integer> neighborValues = new ArrayList<>();
-        neighborValues.add(img[row][col]);
-        int[] nextCardinalDirection = new int[] {0, 1};
-        int[] nextDiagonalDirection = new int[] {1, 1};
-        for (int i = 0; i < 4; i++) {
-            int cardinalRow = row + nextCardinalDirection[0];
-            int cardinalCol = col + nextCardinalDirection[1];
-            int diagonalRow = row + nextDiagonalDirection[0];
-            int diagonalCol = col + nextDiagonalDirection[1];
-
-            boolean cardinalOutOfBounds = cardinalRow < 0 || cardinalRow == img.length || 
-                                          cardinalCol < 0 || cardinalCol == img[0].length;
-            boolean diagonalOutOfBounds = diagonalRow < 0 || diagonalRow == img.length ||
-                                          diagonalCol < 0 || diagonalCol == img[0].length;
-            
-            if (!cardinalOutOfBounds) neighborValues.add(img[cardinalRow][cardinalCol]);
-            if (!diagonalOutOfBounds) neighborValues.add(img[diagonalRow][diagonalCol]);
-
-            int temp = nextCardinalDirection[0];
-            nextCardinalDirection[0] = nextCardinalDirection[1];
-            nextCardinalDirection[1] = -temp;
-            temp = nextDiagonalDirection[0];
-            nextDiagonalDirection[0] = nextDiagonalDirection[1];
-            nextDiagonalDirection[1] = -temp;
+        for (int neighborRow = row - 1; neighborRow <= row + 1; neighborRow++) {
+            for (int neighborCol = col - 1; neighborCol <= col + 1; neighborCol++) {
+                if (neighborRow >= 0 && neighborRow != img.length &&
+                    neighborCol >= 0 && neighborCol != img[0].length)
+                    neighborValues.add(img[neighborRow][neighborCol]);
+            }
         }
         return neighborValues;
     }
