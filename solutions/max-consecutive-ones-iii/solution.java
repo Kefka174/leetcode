2ea62cca76eb503f91f448bbabc6d16c366f7d66
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-08-01 14:33:55 -0500

    Added Sliding Window solution to "1004. Max Consecutive Ones III"

--- solutions/max-consecutive-ones-iii/solution.java
+++ solutions/max-consecutive-ones-iii/solution.java
@@ -1,6 +1,21 @@
 import java.util.Deque;
 
 class Solution {
+    // For each num either grow the size of the sliding window or slide it
+    public int longestOnesSlidingWindow(int[] nums, int k) {
+        int windowStart = 0, zeroesInWindow = 0;
+        for (int num : nums) {
+            if (num == 0) zeroesInWindow++;
+            if (zeroesInWindow > k) {
+                if (nums[windowStart] == 0) zeroesInWindow--;
+                windowStart++;
+            }
+        }
+        return nums.length - windowStart;
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
     public int longestOnes(int[] nums, int k) {
         Deque<Integer> zeroQueue = new ArrayDeque<>(k + 1);
         int onesSinceLastZero = 0;
