49e2335b5ab66d4f22a4c9230a0538df4c83e701
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-08-16 14:31:18 -0500

    Added Counting Sort solution to "215. Kth Largest Element in an Array"

--- solutions/kth-largest-element-in-an-array/solution.java
+++ solutions/kth-largest-element-in-an-array/solution.java
@@ -1,6 +1,26 @@
 import java.util.PriorityQueue;
 
 class Solution {
+    public int findKthLargestCountingSort(int[] nums, int k) { // O(n + (max - min))
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+
+        int[] numCounts = new int[max - min + 1];
+        for (int num : nums) numCounts[num - min]++;
+
+        for (int i = numCounts.length - 1; i >= 0; i--) {
+            if (numCounts[i] >= k) return i + min;
+            k -= numCounts[i];
+        }
+        return 0; // unreachable
+    }
+
+
+    /////////////////////////////////////////////////////////////////
     public int findKthLargest(int[] nums, int k) { // O(n * logk)
         PriorityQueue<Integer> heap = new PriorityQueue<>(k + 1);
         for (int num : nums) {
