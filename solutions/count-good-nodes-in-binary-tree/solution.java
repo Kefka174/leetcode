16583a97aaf82b03916fe621068eea9608363a72
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-08-30 16:53:52 -0500

    Added recursive solution to "1448. Count Good Nodes in Binary Tree"

--- solutions/count-good-nodes-in-binary-tree/solution.java
+++ solutions/count-good-nodes-in-binary-tree/solution.java
@@ -2,7 +2,26 @@ import java.util.Deque;
 import javafx.util.Pair;
 
 class Solution {
-    public int goodNodes(TreeNode root) {
+    public int goodNodesRecursiveDFS(TreeNode root) {
+        return recursiveHelper(root, Integer.MIN_VALUE);
+    }
+
+    private int recursiveHelper(TreeNode node, int maxValue) {
+        int goodNodeCount = 0;
+        if (node.val >= maxValue) {
+            goodNodeCount++;
+            maxValue = node.val;
+        }
+        if (node.left != null)
+            goodNodeCount += recursiveHelper(node.left, maxValue);
+        if (node.right != null)
+            goodNodeCount += recursiveHelper(node.right, maxValue);
+        return goodNodeCount;
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    public int goodNodesIterativeDFS(TreeNode root) {
         int goodNodeCount = 0;
         Deque<Pair<TreeNode, Integer>> nodeStack = new ArrayDeque<>();
         nodeStack.push(new Pair(root, Integer.MIN_VALUE));
