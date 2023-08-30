8f012ed675f0600ee8cf731b4e511744aa2081fb
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-08-29 19:38:22 -0500

    Added Monotonic Deque solution to "239. Sliding Window Maximum"

--- solutions/sliding-window-maximum/solution.java
+++ solutions/sliding-window-maximum/solution.java
@@ -1,8 +1,27 @@
+import java.util.Deque;
 import java.util.Map;
 import java.util.TreeMap;
 
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) { // O(n*logk)
+    public int[] maxSlidingWindowMonotonic(int[] nums, int k) { // O(n)
+        int[] windowMaxes = new int[nums.length - k + 1];
+        Deque<Integer> monotonic = new ArrayDeque<>(k);
+        for (int i = 0; i < nums.length; i++) {
+            if (i >= k && monotonic.peek() == i - k) 
+                monotonic.poll();
+
+            while (!monotonic.isEmpty() && nums[monotonic.peekLast()] <= nums[i]) 
+                monotonic.pollLast();
+
+            monotonic.add(i);
+            if (i >= k - 1) windowMaxes[i - k + 1] = nums[monotonic.peek()];
+        }
+        return windowMaxes;
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    public int[] maxSlidingWindowSorted(int[] nums, int k) { // O(n*logk)
         int[] windowMaxes = new int[nums.length - k + 1];
         TreeMap<Integer, Integer> window = new TreeMap<>();
         for (int i = 0; i < nums.length; i++) {
