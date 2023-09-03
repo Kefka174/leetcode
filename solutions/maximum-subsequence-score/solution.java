c4a901f66c5a672e3fbed2a1ad6fe9c5b76c54e3
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-09-02 19:42:17 -0500

    Refactored "2542. Maximum Subsequence Score"

--- solutions/maximum-subsequence-score/solution.java
+++ solutions/maximum-subsequence-score/solution.java
@@ -5,6 +5,8 @@ import java.util.TreeMap;
 import javafx.util.Pair;
 
 class Solution {
+    // Uses frequency maps in place of multisets
+    // Could use a heap to minimize number of passes from (2*nlogn) to (nlogn + nlogk)
     public long maxScore(int[] nums1, int[] nums2, int k) { // O(n*logn)
         TreeMap<Integer, Integer> sortedNums1 = new TreeMap<>(Collections.reverseOrder());
         for (int num : nums1) sortedNums1.merge(num, 1, Integer::sum);
@@ -14,49 +16,44 @@ class Solution {
             sortedPairs[i] = new Pair(nums2[i], nums1[i]);
         Arrays.sort(sortedPairs, (a, b) -> a.getKey() - b.getKey());
         
-        Map<Integer, Integer> kLargest = new HashMap<>();
-        int numsNeeded = k - 1;
-        while (numsNeeded > 0) {
-            int largestNum = sortedNums1.firstKey();
-            int largestNumFrequency = sortedNums1.get(largestNum);
-            if (largestNumFrequency > numsNeeded) {
-                kLargest.put(largestNum, numsNeeded);
-                sortedNums1.put(largestNum, largestNumFrequency - numsNeeded);
-            }
-            else {
-                kLargest.put(largestNum, largestNumFrequency);
-                sortedNums1.remove(largestNum);
-            }
-            numsNeeded -= largestNumFrequency;
-        }
-        long kLargestSum = 0;
-        for (int num : kLargest.keySet()) 
-            kLargestSum += ((long)num * kLargest.get(num));
-
+        long kLargestSum = sumOfKLargest(sortedNums1, k - 1);
         long max = Long.MIN_VALUE;
         for (int i = 0; i < sortedPairs.length - k + 1; i++) {
             int multiplier = sortedPairs[i].getKey();
             int firstNum = sortedPairs[i].getValue();
 
-            if (sortedNums1.containsKey(firstNum)) {
-                sortedNums1.merge(firstNum, -1, Integer::sum);
-                if (sortedNums1.get(firstNum) == 0) sortedNums1.remove(firstNum);
-            }
-            else {
-                kLargestSum -= firstNum;
-                
-                kLargest.merge(firstNum, -1, Integer::sum);
-                if (kLargest.get(firstNum) == 0) sortedNums1.remove(firstNum);
-                
-                int nextLargest = sortedNums1.firstKey();
-                kLargestSum += nextLargest;
-                sortedNums1.merge(nextLargest, -1, Integer::sum);
-                if (sortedNums1.get(nextLargest) == 0) sortedNums1.remove(nextLargest);
-                kLargest.merge(nextLargest, 1, Integer::sum);
-            }
+            kLargestSum = updateKLargestSum(sortedNums1, firstNum, kLargestSum);
             long currentScore = (firstNum + kLargestSum) * multiplier;
             if (currentScore > max) max = currentScore;
         }
         return max;
+    }
+
+    // Removes val from sortedMap. If val is not in sortedMap, it must be a part of the kLargestSum
+    // so it is removed and the largest val from sortedMap is moved to the kLargestSum.
+    private long updateKLargestSum(TreeMap<Integer, Integer> sortedMap, int val, long kLargestSum) {
+        if (sortedMap.containsKey(val)) 
+            frequenyMapRemove(sortedMap, val);
+        else {
+            kLargestSum -= val;
+            kLargestSum += sortedMap.firstKey();
+            frequenyMapRemove(sortedMap, sortedMap.firstKey());
+        }
+        return kLargestSum;
+    }
+
+    // Removes the k largest ints from a sorted map and returns their sum
+    private long sumOfKLargest(TreeMap<Integer, Integer> sortedMap, int k) {
+        long kLargest = 0;
+        for (int i = 0; i < k; i++) {
+            kLargest += sortedMap.firstKey();
+            frequenyMapRemove(sortedMap, sortedMap.firstKey());
+        }
+        return kLargest;
+    }
+
+    private <T> void frequenyMapRemove(Map<T, Integer> map, T key) {
+        map.merge(key, -1, Integer::sum);
+        if (map.get(key) <= 0) map.remove(key);
     }
 }
