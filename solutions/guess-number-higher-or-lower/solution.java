640a0c5fb5832614b53ec9761e8293c685d4cae0
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-09-05 15:49:57 -0500

    Added Ternary Search solution to "374. Guess Number Higher or Lower"

--- solutions/guess-number-higher-or-lower/solution.java
+++ solutions/guess-number-higher-or-lower/solution.java
@@ -8,10 +8,34 @@
  */
 
 public class Solution extends GuessGame {
-    public int guessNumber(int n) {
+    public int guessNumberTernarySearch(int n) {
         int bottomIndex = 1, topIndex = n;
         while (bottomIndex <= topIndex) {
-            int middleIndex = (int)(((long)topIndex + bottomIndex) / 2);
+            int oneThirdIndex = bottomIndex + (topIndex - bottomIndex) / 3;
+            int twoThirdsIndex = bottomIndex + 2 * ((topIndex - bottomIndex) / 3);
+            int oneThirdGuess = guess(oneThirdIndex);
+            int twoThirdsGuess = guess(twoThirdsIndex);
+
+            if (oneThirdGuess == 0) return oneThirdIndex;
+            if (twoThirdsGuess == 0) return twoThirdsIndex;
+
+            if (oneThirdGuess == -1) topIndex = oneThirdIndex - 1;
+            else if (twoThirdsGuess == 1) bottomIndex = twoThirdsIndex + 1;
+            else {
+                topIndex = twoThirdsIndex - 1;
+                bottomIndex = oneThirdIndex + 1;
+            }
+
+        }
+        return -1; // unreachable
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    public int guessNumberBinarySearch(int n) {
+        int bottomIndex = 1, topIndex = n;
+        while (bottomIndex <= topIndex) {
+            int middleIndex = bottomIndex + (topIndex - bottomIndex) / 2;
             int middleGuess = guess(middleIndex);
 
             if (middleGuess == 0) return middleIndex;
