7291863b740118de8872fdb3961e96a715095a41
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-09-08 13:22:57 -0500

    Updated "1679. Max Number of K-Sum Pairs" to O(n)

--- solutions/max-number-of-k-sum-pairs/solution.java
+++ solutions/max-number-of-k-sum-pairs/solution.java
@@ -1,7 +1,29 @@
 import java.util.Arrays;
+import java.util.Map;
 
 class Solution {
-    public int maxOperations(int[] nums, int k) { // O(nlogn)
+    public int maxOperationsMap(int[] nums, int k) { // O(n)
+        Map<Integer, Integer> frequencies = new HashMap<>();
+        int operationCount = 0;
+        for (int num : nums) {
+            if (frequencies.containsKey(num)) {
+                operationCount++;
+                frequenyMapRemove(frequencies, num);
+            }
+            else 
+                frequencies.merge(k - num, 1, Integer::sum);
+        }
+        return operationCount;
+    }
+
+    private <T> void frequenyMapRemove(Map<T, Integer> map, T item) {
+        map.merge(item, -1, Integer::sum);
+        if (map.get(item) == 0) map.remove(item);
+    }
+
+
+    /////////////////////////////////////////////////////////////////
+    public int maxOperationsSort(int[] nums, int k) { // O(nlogn)
         Arrays.sort(nums);
         int operationCount = 0;
         int leftIndex = 0, rightIndex = nums.length - 1;
