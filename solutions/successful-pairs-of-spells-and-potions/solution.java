3e3a6e8dd76ce6d0cb0ae853486d2195fab2c937
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-09-13 11:31:12 -0500

    Refactored "2300. Successful Pairs of Spells and Potions"

--- solutions/successful-pairs-of-spells-and-potions/solution.java
+++ solutions/successful-pairs-of-spells-and-potions/solution.java
@@ -1,22 +1,29 @@
 import java.util.Arrays;
-import java.util.TreeMap;
 
 class Solution {
     // O(m*logm + n*logm)
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
         int[] sortedPotions = potions.clone();
         Arrays.sort(sortedPotions);
-        TreeMap<Integer, Integer> numPotionsGreaterThanKey = new TreeMap<>();
-        for (int i = 0; i < sortedPotions.length; i++)
-            numPotionsGreaterThanKey.putIfAbsent(sortedPotions[i], sortedPotions.length - i);
 
         int[] spellPairCounts = new int[spells.length];
         for (int i = 0; i < spells.length; i++) {
             int potionStrengthNeeded = (int)Math.ceil(success / (double)spells[i]);
-            Integer position = numPotionsGreaterThanKey.ceilingKey(potionStrengthNeeded);
-            if (position == null) spellPairCounts[i] = 0;
-            else spellPairCounts[i] = numPotionsGreaterThanKey.get(position);
+            int potionIndex = binarySearchIndex(sortedPotions, potionStrengthNeeded);
+            spellPairCounts[i] = potions.length - potionIndex;
         }
         return spellPairCounts;
+    }
+
+    private int binarySearchIndex(int[] arr, int target) {
+        int leftIndex = 0, rightIndex = arr.length - 1;
+        while (leftIndex <= rightIndex) {
+            int middleIndex = (leftIndex + rightIndex) / 2;
+            if (arr[middleIndex] >= target) 
+                rightIndex = middleIndex - 1;
+            else
+                leftIndex = middleIndex + 1;
+        }
+        return leftIndex;
     }
 }
