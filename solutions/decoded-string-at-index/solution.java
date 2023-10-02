e2a0caa8ae2e50851b649c0450e3ab93a5ca987b
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-10-01 19:24:31 -0500

    Refactored "880. Decoded String at Index"

--- solutions/decoded-string-at-index/solution.java
+++ solutions/decoded-string-at-index/solution.java
@@ -1,46 +1,18 @@
 import java.util.Deque;
 
 class Solution {
-    public String decodeAtIndex(String s, int k) { // O(s.length)
-        Deque<StringBuilder> stringStack = new ArrayDeque<>();
+    public String decodeAtIndex(String s, int k) {
         Deque<Long> lengthStack = new ArrayDeque<>();
-        StringBuilder currentString = new StringBuilder();
-        int index = 0;
         long decodedLength = 0;
         while (decodedLength < k) {
-            if (!Character.isDigit(s.charAt(index))) {
-                currentString.append(s.charAt(index));
-                decodedLength++;
-                if (decodedLength == k) 
-                    return s.substring(index, index + 1);
-            }
-            else { // char at index is a digit
-                if (!currentString.isEmpty()) {
-                    stringStack.push(currentString);
-                    lengthStack.push(decodedLength);
-                    currentString = new StringBuilder();
-                }
-                decodedLength *= s.charAt(index) - '0';
-                if (decodedLength < k) {
-                    stringStack.push(new StringBuilder());
-                    lengthStack.push(decodedLength);
-                }
-            }
-            index++;
+            if (!Character.isDigit(s.charAt(lengthStack.size()))) decodedLength++;
+            else decodedLength *= s.charAt(lengthStack.size()) - '0';
+            lengthStack.push(decodedLength);
         }
         
-        while (!stringStack.isEmpty()) {
-            if (!currentString.isEmpty() && k > lengthStack.peek() && 
-                k - lengthStack.peek() <= currentString.length()) {
-                k -= lengthStack.peek();
-                return currentString.substring(k - 1, k);
-            }
-
-            k %= lengthStack.peek();
-            if (k == 0) k = lengthStack.pop().intValue();
-            else lengthStack.pop();
-            currentString = stringStack.pop();
-        }
-        return currentString.substring(k - 1, k);
+        while (k != 0 || Character.isDigit(s.charAt(lengthStack.size())))
+            k %= lengthStack.pop();
+        
+        return s.substring(lengthStack.size(), lengthStack.size() + 1);
     }
 }
