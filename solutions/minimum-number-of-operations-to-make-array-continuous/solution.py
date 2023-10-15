7353ac035e78bc5a40cbfbc33da15a4dfdda6d7b
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-10-14 22:29:38 -0500

    Refactored "2009. Minimum Number of Operations to Make Array Continuous"

--- solutions/minimum-number-of-operations-to-make-array-continuous/solution.py
+++ solutions/minimum-number-of-operations-to-make-array-continuous/solution.py
@@ -3,22 +3,11 @@ from typing import List
 class Solution:
     def minOperations(self, nums: List[int]) -> int: # O(nlogn)
         sortedUniqueNums = sorted(set(nums))
-        startIndex, endIndex = 0, 0
-
-        while (endIndex < len(sortedUniqueNums) and 
-               sortedUniqueNums[endIndex] <= sortedUniqueNums[0] + (len(nums) - 1)):
-            endIndex += 1
-
-        if endIndex == len(sortedUniqueNums):
-            return len(nums) - len(sortedUniqueNums)
-        
-        maxWindowCount = endIndex
-        while endIndex < len(sortedUniqueNums) - 1:
-            endIndex += 1
+        startIndex, maxWindowCount = 0, 0
+        for endIndex in range(len(sortedUniqueNums)):
             while sortedUniqueNums[startIndex] < sortedUniqueNums[endIndex] - (len(nums) - 1):
                 startIndex += 1
-            
-            if (endIndex - startIndex) + 1 > maxWindowCount:
-                maxWindowCount = (endIndex - startIndex) + 1
+                
+            maxWindowCount = max(maxWindowCount, (endIndex - startIndex) + 1)
 
         return len(nums) - maxWindowCount
