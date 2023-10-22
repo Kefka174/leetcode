39de55625839ce04916148820d7fc3b863c8a6b5
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-10-22 08:24:12 -0500

    Added Counting Sort solution to "274. H-Index"

--- solutions/h-index/solution.py
+++ solutions/h-index/solution.py
@@ -2,7 +2,20 @@ from typing import List
 from heapq import heapify, heappop
 
 class Solution:
-    def hIndex(self, citations: List[int]) -> int: # O(nlogn)
+    def hIndexCountingSort(self, citations: List[int]) -> int: # O(n)
+        citeCounts = [0] * (len(citations) + 1)
+        for cite in citations:
+            citeCounts[min(cite, len(citations))] += 1
+
+        papersInH = 0
+        for hVal in range(len(citations), -1, -1):
+            papersInH += citeCounts[hVal]
+            if papersInH >= hVal:
+                return hVal
+
+
+    #################################################################
+    def hIndexHeap(self, citations: List[int]) -> int: # O(nlogn)
         maxH = 0
         citationHeap = citations.copy()
         heapify(citationHeap)
