d1ff76a814e9ce8737d61bdaf0bd86d282c373df
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-10-29 07:53:36 -0500

    Refactored "779. K-th Symbol in Grammar"

--- solutions/k-th-symbol-in-grammar/solution.py
+++ solutions/k-th-symbol-in-grammar/solution.py
@@ -1,5 +1,11 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int: # O(log2(k))
+        flipCount = bin(k - 1).count('1')
+        return flipCount % 2
+
+
+    ############################################################
+    def kthGrammarVerbose(self, n: int, k: int) -> int: # O(log2(k))
         flipCount = 0
         rowSize = pow(2, n - 1)
         while k > 1:
