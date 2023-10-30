40b99c0f4bde0436830d96de7a46673c4fd60700
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-10-30 11:40:18 -0500

    New solution - "1356. Sort Integers by The Number of 1 Bits"

---
+++ solutions/sort-integers-by-the-number-of-1-bits/solution.py
@@ -0,0 +1,6 @@
+from typing import List
+
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        arr.sort(key = lambda num: (bin(num).count('1'), num))
+        return arr
