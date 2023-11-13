8872137d304c9ab7925312d35fea7df9c17094da
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-11-13 10:24:53 -0600

    Updated "2785. Sort Vowels in a String" to O(n) time, O(1) space.

--- solutions/sort-vowels-in-a-string/solution.py
+++ solutions/sort-vowels-in-a-string/solution.py
@@ -1,5 +1,26 @@
 class Solution:
-    def sortVowels(self, s: str) -> str:
+    def countingSortVowels(self, s: str) -> str: # O(n) time, O(1) space
+        vowels = ['A', 'E', 'I', 'O', 'U',
+                  'a', 'e', 'i', 'o', 'u']
+        vowelFrequencies = dict.fromkeys(vowels, 0)
+        s = list(s)
+        
+        for char in s:
+            if char in vowels:
+                vowelFrequencies[char] += 1
+
+        vowelIndex = 0
+        for i, char in enumerate(s):
+            if char in vowels:
+                while vowelFrequencies[vowels[vowelIndex]] == 0:
+                    vowelIndex += 1
+                s[i] = vowels[vowelIndex]
+                vowelFrequencies[vowels[vowelIndex]] -= 1
+        return ''.join(s)
+
+
+    ##################################################
+    def sortVowels(self, s: str) -> str: # O(nlogn) time, O(n) space
         vowels = ('A', 'E', 'I', 'O', 'U',
                   'a', 'e', 'i', 'o', 'u')
         s = list(s)
