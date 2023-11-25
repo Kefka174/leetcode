f43e33c8d323164570ccdc35bfa8af37d56f4d51
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-11-25 14:16:07 -0600

    Refactored "1630. Arithmetic Subarrays" to O(m*n)

--- solutions/arithmetic-subarrays/solution.py
+++ solutions/arithmetic-subarrays/solution.py
@@ -1,7 +1,7 @@
 from typing import List
 
 class Solution:
-    # O(m * nlogn)
+    # O(m*n)
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
         subarrayIsArithmetic = [False] * len(r)
         for i in range(len(r)):
@@ -9,9 +9,14 @@ class Solution:
         return subarrayIsArithmetic
     
     def arrIsArithmetic(self, arr: List[int]) -> bool:
-        arr.sort()
-        diff = arr[1] - arr[0]
-        for i in range(2, len(arr)):
-            if arr[i] - arr[i - 1] != diff:
+        minVal, maxVal = min(arr), max(arr)
+        diff = (maxVal - minVal) // (len(arr) - 1)
+        arrSet = set(arr)
+
+        if diff == 0:
+            return len(arrSet) == 1
+        
+        for num in range(minVal, maxVal + 1, diff):
+            if num not in arrSet:
                 return False
         return True
