a17066508bf68a9156c2b5a4c8d0265b5ad9f26f
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2024-02-10 14:58:53 -0600

    Refactored "1582. Special Positions in a Binary Matrix"

--- solutions/special-positions-in-a-binary-matrix/solution.py
+++ solutions/special-positions-in-a-binary-matrix/solution.py
@@ -2,25 +2,16 @@ from typing import List, Set
 
 class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
-        specialRows, normalRows = set(), set()
-        specialCols, normalCols = set(), set()
+        rowVals, colVals = [0] * len(mat), [0] * len(mat[0])
         for row in range(len(mat)):
             for col in range(len(mat[0])):
                 if mat[row][col] == 1:
-                    self.addToSets(row, specialRows, normalRows)
-                    self.addToSets(col, specialCols, normalCols)
+                    rowVals[row] += 1
+                    colVals[col] += 1
 
-        specialCellCount = 0 
+        specialCellCount = 0
         for row in range(len(mat)):
             for col in range(len(mat[0])):
-                if mat[row][col] == 1 and row in specialRows and col in specialCols:
+                if mat[row][col] == rowVals[row] == colVals[col] == 1:
                     specialCellCount += 1
         return specialCellCount
-
-    def addToSets(self, val: int, singleSet: Set[int], overflowSet: Set[int]) -> None:
-        if val not in overflowSet:
-            if val in singleSet:
-                singleSet.remove(val)
-                overflowSet.add(val)
-            else:
-                singleSet.add(val)
