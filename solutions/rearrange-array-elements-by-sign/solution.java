acc97ab79579b4f3dbaef09cbcec070eda1d1fc0
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2024-03-21 08:30:57 -0500

    Added Two Pointer solution to "2149. Rearrange Array Elements by Sign"

--- solutions/rearrange-array-elements-by-sign/solution.java
+++ solutions/rearrange-array-elements-by-sign/solution.java
@@ -15,4 +15,22 @@ class Solution {
         }
         return nums;
     }
+
+    
+    //////////////////////////////////////////////////////////////////////
+    public int[] rearrangeArrayTwoPointer(int[] nums) {
+        int[] rearranged = new int[nums.length];
+        int positiveIndex = 0, negativeIndex = 1;
+        for (int num : nums) {
+            if (num >= 0) {
+                rearranged[positiveIndex] = num;
+                positiveIndex += 2;
+            }
+            else {
+                rearranged[negativeIndex] = num;
+                negativeIndex += 2;
+            }
+        }
+        return rearranged;
+    }
 }
