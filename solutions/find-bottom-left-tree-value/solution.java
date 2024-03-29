2243b57c3a18035280b9414b41d992ecc1e46373
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2024-03-29 11:46:24 -0500

    Refactored "513. Find Bottom Left Tree Value"

--- solutions/find-bottom-left-tree-value/solution.java
+++ solutions/find-bottom-left-tree-value/solution.java
@@ -2,22 +2,15 @@ import java.util.Queue;
 
 class Solution {
     public int findBottomLeftValue(TreeNode root) { // level order traversal
-        Queue<TreeNode> currentLevel = new ArrayDeque<>();
-        Queue<TreeNode> nextLevel = new ArrayDeque<>();
-        nextLevel.add(root);
-        int leftValueOfCurrentLevel = root.val;
-        while (!currentLevel.isEmpty() || !nextLevel.isEmpty()) {
-            if (currentLevel.isEmpty()) {
-                currentLevel = nextLevel;
-                nextLevel = new ArrayDeque<>();
-                leftValueOfCurrentLevel = currentLevel.peek().val;
-            }
-
-            TreeNode node = currentLevel.poll();
-            if (node.left != null) nextLevel.add(node.left);
-            if (node.right != null) nextLevel.add(node.right);
+        Queue<TreeNode> nodeQueue = new ArrayDeque<>();
+        nodeQueue.add(root);
+        TreeNode currentNode = null;
+        while (!nodeQueue.isEmpty()) {
+            currentNode = nodeQueue.poll();
+            if (currentNode.right != null) nodeQueue.add(currentNode.right);
+            if (currentNode.left != null) nodeQueue.add(currentNode.left);
         }
-        return leftValueOfCurrentLevel;
+        return currentNode.val;
     }
 }
 
