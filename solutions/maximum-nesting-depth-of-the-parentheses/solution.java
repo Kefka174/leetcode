8999b1c7d555c35181b84aab9bced789e2b03510
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2024-04-11 21:28:12 -0500

    New solution - "1614. Maximum Nesting Depth of the Parentheses"

---
+++ solutions/maximum-nesting-depth-of-the-parentheses/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxDepth(String s) {
+        int depth = 0, maxDepth = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') depth++;
+            else if (s.charAt(i) == ')') depth--;
+
+            maxDepth = Math.max(depth, maxDepth);
+        }
+        return maxDepth;
+    }
+}
