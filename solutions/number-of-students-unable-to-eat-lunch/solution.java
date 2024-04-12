4d7c46b2c10b76d9b548c4e4f0adb25509470ff0
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2024-04-12 17:56:34 -0500

    Updated "1700. Number of Students Unable to Eat Lunch" to O(n).

--- solutions/number-of-students-unable-to-eat-lunch/solution.java
+++ solutions/number-of-students-unable-to-eat-lunch/solution.java
@@ -1,7 +1,7 @@
 import java.util.Queue;
 
 class Solution {
-    public int countStudents(int[] students, int[] sandwiches) {
+    public int countStudentsSimulation(int[] students, int[] sandwiches) { // O(n^2)
         Queue<Integer> studentQueue = new ArrayDeque<>(students.length);
         for (int student : students) studentQueue.add(student);
         int numStudentsNotEating = 0, sandwichIndex = 0;
@@ -17,5 +17,20 @@ class Solution {
             }
         }
         return numStudentsNotEating;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    public int countStudents(int[] students, int[] sandwiches) { // O(n)
+        int circularStudents = 0, squareStudents = 0;
+        for (int student : students) {
+            if (student == 0) circularStudents++;
+            else squareStudents++;
+        }
+        for (int sandwich : sandwiches) {
+            if (sandwich == 0 && circularStudents > 0) circularStudents--;
+            else if (sandwich == 1 && squareStudents > 0) squareStudents--;
+            else return circularStudents + squareStudents;
+        }
+        return 0;
     }
 }
