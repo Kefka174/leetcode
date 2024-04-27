f0b09ef6c7bde9af3c9d9c0397d10d573027ed57
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2024-04-27 08:31:34 -0500

    New solutions - "1683. Invalid Tweets", "1378. Replace Employee ID With The Unique Identifier", and "1068. Product Sales Analysis I"
    1683. Invalid Tweets
    1378. Replace Employee ID With The Unique Identifier
    1068. Product Sales Analysis I

---
+++ solutions/invalid-tweets/solution.sql
@@ -0,0 +1,13 @@
+-- Table: Tweets
+
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | tweet_id       | int     |
+-- | content        | varchar |
+-- +----------------+---------+
+-- tweet_id is the primary key (column with unique values) for this table.
+-- This table contains all the tweets in a social media app.
+
+SELECT tweet_id FROM Tweets
+WHERE CHAR_LENGTH(content) > 15
