ec3d7867fde8cabd37b5b6dbc58576038719241b
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2024-04-28 09:43:15 -0500

    New solutions - "577. Employee Bonus" and "570. Managers with at Least 5 Direct Reports"
    577. Employee Bonus
    570. Managers with at Least 5 Direct Reports

---
+++ solutions/managers-with-at-least-5-direct-reports/solution.sql
@@ -0,0 +1,18 @@
+-- Table: Employee
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | department  | varchar |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table indicates the name of an employee, their department, and the id of their manager.
+-- If managerId is null, then the employee does not have a manager.
+-- No employee will be the manager of themself.
+
+SELECT e2.name
+FROM Employee e1 JOIN Employee e2 ON e1.managerId = e2.id
+GROUP BY e1.managerId
+HAVING COUNT(*) >= 5
