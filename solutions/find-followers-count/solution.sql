3e9b63d492eccbacf0630e8d97a5ded4d7c73019
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2024-05-04 12:24:50 -0500

    New solutions - "596. Classes More Than 5 Students" and "1729. Find Followers Count"
    596. Classes More Than 5 Students
    1729. Find Followers Count

---
+++ solutions/find-followers-count/solution.sql
@@ -0,0 +1,14 @@
+-- Table: Followers
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | user_id     | int  |
+-- | follower_id | int  |
+-- +-------------+------+
+-- (user_id, follower_id) is the primary key (combination of columns with unique values) for this table.
+-- This table contains the IDs of a user and a follower in a social media app where the follower follows the user.
+
+SELECT user_id, COUNT(*) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id
